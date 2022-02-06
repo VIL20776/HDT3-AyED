c1d26365ef6ef3ae5f539d8bab98d18fd9acff97
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-02-05 19:50:50 -0600

    Add files via upload

--- RadixSort.java
+++ RadixSort.java
@@ -1,60 +1,47 @@
 import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RadixSort {
 	public RadixSort() {
 		
 	}
 	
-	 private static void radixSort(int[] arr){
-		    //get max element in array
-		    int max = getMaxElementInArray(arr);
-		    int position = 1;
-		    // move from least significant digit 
-		    // to most significant digit
-		    while(max/position > 0){
-		      countingSort(arr, position);
-		      position *= 10;
-		    }        
-		  }
-		    
-		  private static int getMaxElementInArray(int[] arr){
-		    int max = arr[0];
-		    for(int i = 1; i < arr.length; i++){
-		      if (arr[i] > max){
-		          max = arr[i];
-		      }
-		    }
-		    return max;
-		  }
-		    
-		  // Counting sort used to sort array in each pass
-		  private static void countingSort(int[] arr, int position){
-		    int n = arr.length;
-		    int[] output = new int[n];
-		    int[] count = new int[n];
-		        
-		    //Calculate frequency of each element, put it in count array
-		    for(int i = 0; i < arr.length; i++){
-		      count[(arr[i]/position)%10]++;
-		    }
-		    // Modify count array to get the final position of elements
-		    for(int i = 1; i < n; i++){
-		      count[i] = count[i] + count[i-1];
-		    }
-		    
-		    // Add elements to output array for this pass
-		    for(int i = n-1; i >=0; i--){
-		      output[count[(arr[i]/position)%10] - 1] = arr[i];
-		      count[(arr[i]/position)%10]--;
-		    }
-		    // Copy output array to the input for 
-		    // the next pass of counting sort
-		    for(int i = 0; i < output.length; i++){
-		      arr[i] = output[i];
-		    }
+	public void radixSort(int[] arr)
+	{
+	    Queue<Integer>[] buckets = new Queue[10];
+	    for (int i = 0; i < 10; i++)
+	        buckets[i] = new LinkedList<Integer>();
 
-		  }
-		}
+	    boolean sorted = false;
+	    int expo = 1;
+
+	    while ( ! sorted) {
+	        sorted = true;
+
+	        for (int item : arr) {
+	            int bucket = (item / expo) % 10;
+	            if (bucket > 0) sorted = false;
+	            buckets[bucket].add(item);
+	        }
+
+	        expo *= 10;
+	        int index = 0;
+
+	        for (Queue<Integer> bucket : buckets)
+	            while ( ! bucket.isEmpty())
+	                arr[index++] = bucket.remove();
+	    }
+
+	    assert isSorted(arr);
+	}
+
+	private boolean isSorted(int[] arr) {
+		for (int i = 1; i< arr.length; i++)
+			return false;
+		return true;
+	}
+}
 	
 
 
