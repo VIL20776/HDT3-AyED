5893ebbc5db9342fa2094231c44f8b54b3efafa7
CarlDanielJ <mar19340@uvg.edu.gt>
2022-02-05 19:48:10 -0600

    Implementacion de heap sort

--- Sorting.java
+++ Sorting.java
@@ -72,4 +72,53 @@ public class Sorting<E extends Comparable<E>>  {
 		      quicksort(list,j+1,der);          // ordenamos subarray derecho
 		   
 		}
+	
+	public void heapSort(E[] list)
+    {
+        int n = list.length;
+  
+        // Build heap (relistange listay)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(list, n, i);
+  
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            E temp = list[0];
+            list[0] = list[i];
+            list[i] = temp;
+  
+            // call max heapify on the reduced heap
+            heapify(list, i, 0);
+        }
+    }
+  
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    void heapify(E [] list, int n, int i)
+    {
+        int largest = i;  // Initialize largest as root
+        int l = 2*i + 1;  // left = 2*i + 1
+        int r = 2*i + 2;  // right = 2*i + 2
+  
+        // If left child is larger than root
+        if (l < n && list[1].compareTo(list[largest])>0)
+            largest = l;
+  
+        // If right child is larger than largest so far
+        if (r < n && list[r].compareTo(list[largest])>0)
+            largest = r;
+  
+        // If largest is not root
+        if (largest != i)
+        {
+            E swap = list[i];
+            list[i] = list[largest];
+            list[largest] = swap;
+  
+            // Recursively heapify the affected sub-tree
+            heapify(list, n, largest);
+        }
+    }
 }
